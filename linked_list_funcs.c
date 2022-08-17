c67a13eec51b29bd108439ec0433bb955e52df8f
Ignacio Castro <ignaciocastro347@gmail.com>
2022-08-17 13:57:48 -0300

    Remove an unused linked list function.

--- linked_list_funcs.c
+++ linked_list_funcs.c
@@ -20,35 +20,6 @@ size_t print_nodes(const stack_t *h)
 }
 
 /**
- * add_node_to_end - add a node to the end of a linked list
- * @head: dlinked list
- * @n: int value
- * Return: pointer to a dlinked list
- */
-stack_t *add_node_to_end(stack_t **head, const int n)
-{
-	stack_t *aux, *new;
-
-	new = malloc(sizeof(stack_t));
-	if (!new)
-		return (NULL);
-	new->n = n;
-	new->next = NULL;
-	if (*head)
-	{
-		aux = *head;
-		while (aux->next)
-			aux = aux->next;
-		aux->next = new;
-		new->prev = aux;
-		return (new);
-	}
-	new->prev = NULL;
-	*head = new;
-	return (new);
-}
-
-/**
  * add_node_to_beg - add a new node at the beggining of a list
  * @head: linked list
  * @n: int value
